2464c7e9d8a294be8775a3a4f0c5dbc5ebe3a0d4
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-11-10 21:44:47 +0430

    Create custom-hook for displaying the initial data

---
+++ src/hooks/useCountries.js
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { getAllCountries } from "../services/apiServices";
+
+export const useCountries = () => {
+  const [countries, setCountries] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getAllCountries();
+        setCountries(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return {countries, isLoading, error}
+};
