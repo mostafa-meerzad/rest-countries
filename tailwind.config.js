ab8c624e7767aabf07dd792c93d48340cc264608
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-11-10 09:09:33 +0430

    Remove boilerplate code

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,19 @@
 export default {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBlue: "hsl(209, 23%, 22%)",
+        veryDarkBlueDarker: "hsl(207, 26%, 17%)",
+        veryDarkBlue: "hsl(200, 15%, 8%)",
+        darkGray: "hsl(0, 0%, 52%)",
+        veryLightGray: "hsl(0, 0%, 98%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+      fontFamily: {
+        Nunito: ["Nunito Sans", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
