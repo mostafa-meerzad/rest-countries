1626047f27a4dc7eb9e9660ca0dcc41c3fa23396
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-11-10 21:44:08 +0430

    Create data fetching logic

---
+++ src/services/apiServices.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const BASE_URL = "https://restcountries.com/v3.1";
+
+export const getAllCountries = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}/all`);
+    return response.data;
+  } catch (error) {
+    console.log("something went wrong getting all countries ");
+    throw error;
+  }
+};
+
+export const getCountry = (name) => {
+  try {
+    const response = axios.get(`${BASE_URL}/name/${name}`);
+    return response.data;
+  } catch (error) {
+    console.log("something went wrong getting a country");
+    throw error;
+  }
+};
