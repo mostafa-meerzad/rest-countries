c6e2ec9a0788ab2177e4f97a8374be1aacb1242a
mostafa-meerzad <71199539+mostafa-meerzad@users.noreply.github.com>
2023-11-13 13:13:34 +0430

    Complete loading and error handling

---
+++ src/components/Error.jsx
@@ -0,0 +1,10 @@
+import EmptyFolder from "./EmptyFolder"
+
+const Error = () => {
+  return (
+    <div className="grid content-center w-full py-12 ">
+        <EmptyFolder className={`w-32 h-32 mx-auto stroke-gray-400 dark:stroke-gray-300 `}/>
+    </div>
+  )
+}
+export default Error
--- src/sections/Main.jsx
+++ src/sections/Main.jsx
@@ -2,35 +2,54 @@ import Country from "../components/Country";
 import { useCountries } from "../hooks/useCountries";
 import Dropdown from "../components/Dropdown";
 import Search from "../components/Search";
+import Spinner from "../components/Spinner";
+import { motion } from "framer-motion";
+import Error from "../components/Error";
 
 const Main = () => {
-  const { countries, isLoading, error, filterCountries, searchCountry } = useCountries();
+  const { countries, isLoading, error, filterCountries, searchCountry } =
+    useCountries();
+
   return (
-    <main className="max-w-screen-2xl mx-auto">
+    <main className="max-w-screen-2xl mx-auto relative">
+      <motion.div
+        className={`fixed grid content-center w-full h-screen top-0 left-0  bg-darkBlue  bg-opacity-50  z-10 `}
+        initial={{ opacity: 0, zIndex: -10 }}
+        animate={
+          isLoading ? { opacity: 1, zIndex: 10 } : { opacity: 0, zIndex: -10 }
+        }
+      >
+        <Spinner className="w-8 h-8 mx-auto dark:fill-veryLightGray animate-spin " />
+      </motion.div>
+
       <header className="flex flex-col md:flex-row justify-between items-start md:items-center gap-8 w-full px-4 md:px-8 xl:px-12  my-10 ">
         <Search setSearch={searchCountry} />
-        <Dropdown setFilter={filterCountries}/>
+        <Dropdown setFilter={filterCountries} />
       </header>
       <div className=" px-4 md:px-8 xl:px-12  grid gap-8 md:grid-flow-row md:grid-cols-2 xl:grid-cols-4">
-        {countries.map(
-          ({
-            name: { official },
-            region,
-            capital: [capital] = "unknown",
-            population,
-            flags: { svg: flagUrl },
-          }) => {
-            return (
-              <Country
-                key={official}
-                name={official}
-                region={region}
-                capital={capital}
-                population={population}
-                imgUrl={flagUrl}
-              />
-            );
-          }
+        {error ? (
+          <Error />
+        ) : (
+          countries.map(
+            ({
+              name: { official },
+              region,
+              capital: [capital] = "unknown",
+              population,
+              flags: { svg: flagUrl },
+            }) => {
+              return (
+                <Country
+                  key={official}
+                  name={official}
+                  region={region}
+                  capital={capital}
+                  population={population}
+                  imgUrl={flagUrl}
+                />
+              );
+            }
+          )
         )}
       </div>
     </main>
