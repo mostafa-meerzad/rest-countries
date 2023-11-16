59f4e9cc6a93cbca893880708f422a17a88ae8e2
mostafa <mostafameerzad@gmail.com>
2023-11-16 07:25:58 +0430

    Bugfix fix the initial theme in the useTheme hook

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -28,8 +28,10 @@ export const useTheme = () => {
         window.matchMedia("(prefers-color-scheme: dark)").matches)
     ) {
       document.documentElement.classList.add("dark");
+      setTheme("dark")
     } else {
       document.documentElement.classList.remove("dark");
+      setTheme("light")
     }
   }, []);
 
